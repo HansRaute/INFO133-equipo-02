2c5f2352e2f4f2efa8e77abf8bd0d911fcb84615
sepé_17 <63886995+cperez17@users.noreply.github.com>
2023-07-07 13:43:06 -0400

    Add files via upload

--- insert.py
+++ insert.py
@@ -8,15 +8,6 @@ def insert_media():
         database="your_database"
     )
 
-    cursor = db.cursor()
-    print("Indica la información del medio que quieres añadir con el siguiente formato: nombre, ciudad, región, país, continente, año de creación")
-    media_data = input().split(', ')
-
-    add_media = ("INSERT INTO MedioDePrensa "
-                 "(Nombre, Ciudad, Region, Pais, Continente, AnioDeFundacion) "
-                 "VALUES (%s, %s, %s, %s, %s, %s)")
-    cursor.execute(add_media, (media_data[0], media_data[1], media_data[2], media_data[3], media_data[4], int(media_data[5])))
-    media_id = cursor.lastrowid
 
     print("Indica la información del fundador con el siguiente formato: nombre, fecha de nacimiento")
     founder_data = input().split(', ')
@@ -24,13 +15,30 @@ def insert_media():
                    "(Nombre, FechaDeNacimiento, FK_MedioDePrensa) "
                    "VALUES (%s, %s, %s)")
     cursor.execute(add_founder, (founder_data[0], founder_data[1], media_id))
-    
+
+
+     print("Indica la información de la noticia con el siguiente formato: título, contenido, URL, fecha, ID del medio de prensa")
+    news_data = input().split(', ')
+
+    add_news = ("INSERT INTO Noticia "
+                "(XPATH_titulo, XPATH_contenido, XPATH_url, XPATH_fecha, FK_MedioDePrensa) "
+                "VALUES (%s, %s, %s, %s, %s)")
+    cursor.execute(add_news, (news_data[0], news_data[1], news_data[2], news_data[3], int(news_data[4])))
+
+    db.commit()
+    cursor.close()
+    db.close()
+
+
     print("Indica la información de la ubicación con el siguiente formato: región, país, ciudad, continente")
     location_data = input().split(', ')
     add_location = ("INSERT INTO Ubicacion "
                     "(Region, Pais, Ciudad, Continente, FK_MedioDePrensa) "
                     "VALUES (%s, %s, %s, %s, %s)")
     cursor.execute(add_location, (location_data[0], location_data[1], location_data[2], location_data[3], media_id))
+
+
+
 
     print("Indica la información de la red social con el siguiente formato: nombre, número de seguidores, plataforma")
     social_data = input().split(', ')
@@ -42,6 +50,17 @@ def insert_media():
     db.commit()
     cursor.close()
     db.close()
+  
+    cursor = db.cursor()
+    print("Indica la información del medio que quieres añadir con el siguiente formato: nombre, ciudad, región, país, continente, año de creación")
+    media_data = input().split(', ')
+
+    add_media = ("INSERT INTO MedioDePrensa "
+                 "(Nombre, Ciudad, Region, Pais, Continente, AnioDeFundacion) "
+                 "VALUES (%s, %s, %s, %s, %s, %s)")
+    cursor.execute(add_media, (media_data[0], media_data[1], media_data[2], media_data[3], media_data[4], int(media_data[5])))
+    media_id = cursor.lastrowid
+
 
 def insert_news():
     db = mysql.connector.connect(
@@ -52,17 +71,7 @@ def insert_news():
     )
 
     cursor = db.cursor()
-    print("Indica la información de la noticia con el siguiente formato: título, contenido, URL, fecha, ID del medio de prensa")
-    news_data = input().split(', ')
-
-    add_news = ("INSERT INTO Noticia "
-                "(XPATH_titulo, XPATH_contenido, XPATH_url, XPATH_fecha, FK_MedioDePrensa) "
-                "VALUES (%s, %s, %s, %s, %s)")
-    cursor.execute(add_news, (news_data[0], news_data[1], news_data[2], news_data[3], int(news_data[4])))
-
-    db.commit()
-    cursor.close()
-    db.close()
+    
 
 if __name__ == "__main__":
     insert_media()
